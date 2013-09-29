4b7eb052022a27325a2b4d0b0f806bed3028fa02
Hayden Faulds <hayden.faulds@bt.com>
2013-09-29 20:15:22 +0100

    play around with hps

--- public/test.js
+++ public/test.js
@@ -4,7 +4,10 @@
   window.requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame;
 
   var pitchElem = null;
-  var frequencySpectrum = null;
+  var freqSpectrum0 = null;
+  var freqSpectrum1 = null;
+  var freqSpectrum2 = null;
+  var freqSpectrum3 = null;
 
   var audioContext = new AudioContext();
   var analyser = null;
@@ -14,7 +17,10 @@
 
   window.onload = function() {
     pitchElem = document.getElementById('pitch');
-    frequencySpectrum = document.getElementById('frequencySpectrum').getContext('2d');
+    freqSpectrum0 = document.getElementById('freqSpectrum0').getContext('2d');
+    freqSpectrum1 = document.getElementById('freqSpectrum1').getContext('2d');
+    freqSpectrum2 = document.getElementById('freqSpectrum2').getContext('2d');
+    freqSpectrum3 = document.getElementById('freqSpectrum3').getContext('2d');
     requestUserMedia({audio:true}, gotStream);
   }
 
@@ -47,42 +53,89 @@
     render();
   }
 
-  function findLoudestFreq() {
+  function findLoudestFreq(series) {
     var i = 0;
-    var maxAmplitude = freqData[0];
+    var maxAmplitude = series[0];
     var maxAmplitudeFreq = 0;
 
-    while(++i < freqData.length) {
-      if(freqData[i] > maxAmplitude) {
+    while(++i < series.length) {
+      if(series[i] > maxAmplitude) {
         maxAmplitudeFreq = i;
-        maxAmplitude = freqData[i];
+        maxAmplitude = series[i];
       }
     }
 
     return maxAmplitudeFreq;
   }
 
-  function renderFrequencySpectrum(maxAmplitudeFreq) {
-    var i = 0;
-    frequencySpectrum.strokeStyle = "rgb(0,0,0)";
-    while(++i < freqData.length) {
-      if(i == maxAmplitudeFreq) {
-        frequencySpectrum.strokeStyle = "rgb(200,0,0)";
+  function downSample(series, scale) {
+    var newSampleCount = series.length / scale;
+    var downSampledSeries = new Float32Array(newSampleCount);
+
+    for(var i=0; i < newSampleCount; i++) {
+      var downSampledValue = 0;
+      for(var j=0; j < scale; j++) {
+        downSampledValue += series[i*scale+j];
       }
-      frequencySpectrum.beginPath();
-      frequencySpectrum.moveTo(i, 0);
-      frequencySpectrum.lineTo(i, (freqData[i] - analyser.minDecibels) * 2);
-      frequencySpectrum.stroke();
+      downSampledSeries[i] = downSampledValue / scale;
+    }
+    return downSampledSeries;
+  }
+
+  function renderFrequencySpectrum(canvas, series, maxAmplitudeFreq, scale) {
+    canvas.setTransform(1, 0, 0, 1, 0, 0);
+    canvas.clearRect(0, 0, 2048, 10000);
+    var i = 0;
+    canvas.strokeStyle = "rgb(0,0,0)";
+    while(++i < series.length) {
+      if(i == maxAmplitudeFreq) {
+        canvas.strokeStyle = "rgb(200,0,0)";
+      }
+      canvas.beginPath();
+      canvas.moveTo(i, 0);
+      canvas.lineTo(i, series[i] * scale);
+      canvas.stroke();
     }
   }
 
+  function productOf() {
+    var series = arguments;
+    var samples = series[0].length;
+
+    var product = new Float32Array(series[0]);
+
+    for(var i=1; i < series.length; i++) {
+      for(var j=0; j < samples; j++) {
+        product[j] *= series[i][j];
+      }
+    }
+
+    return product;
+  }
+
   function render(time) {
-    frequencySpectrum.setTransform(1, 0, 0, 1, 0, 0);
-    frequencySpectrum.clearRect(0, 0, 2048, 400);
     analyser.getFloatFrequencyData(freqData);
 
-    maxAmplitudeFreq = findLoudestFreq();
-    renderFrequencySpectrum(maxAmplitudeFreq);
+    for(var i=0; i < freqData.length; i++) {
+      var f = (freqData[i] - analyser.minDecibels) * 
+        (analyser.maxDecibels - analyser.minDecibels) *
+        0.01;
+      if (f<0) f=0; 
+      freqData[i] = f;
+    }
+
+    freqData2 = downSample(freqData, 2);
+    freqData3 = downSample(freqData, 3);
+
+    hsp = productOf(freqData, freqData2, freqData3);
+
+    maxAmplitudeFreq = findLoudestFreq(hsp);
+
+    renderFrequencySpectrum(freqSpectrum0, freqData, maxAmplitudeFreq, 4);
+    renderFrequencySpectrum(freqSpectrum1, freqData2, maxAmplitudeFreq, 4);
+    renderFrequencySpectrum(freqSpectrum2, freqData3, maxAmplitudeFreq, 4);
+
+    renderFrequencySpectrum(freqSpectrum3, hsp, maxAmplitudeFreq, 0.1);
 
     pitchElem.innerText = maxAmplitudeFreq;
 
